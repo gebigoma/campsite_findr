bfe60cf6241bc4e4f87f75d78747413171ad846e
Tina <tparkmedia@gmail.com>
2018-06-11 12:28:28 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'sites/index'
-  get 'sites/show'
-  get 'sites/new'
-  get 'sites/create'
-  get 'sites/edit'
-  get 'sites/update'
-  get 'sites/destroy'
 root 'users#index'
 resources :users
+resources :sites
+
 
 resources :sessions, only: [:new, :create]
 delete '/logout' => 'sessions#destroy', as: :logout
