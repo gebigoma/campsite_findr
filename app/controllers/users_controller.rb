94722890cb04790b12d57ac26e74383753b587de
Tina <tparkmedia@gmail.com>
2018-06-11 16:07:47 -0700

    added link to site in user profile and delete user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    unless current_user.id == @user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def new
