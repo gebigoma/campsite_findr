afe8e941b20fdd332809e1a5f871fbd6008304f7
Tina <tparkmedia@gmail.com>
2018-06-12 09:40:55 -0700

    fix migration

--- db/migrate/20180611212110_add_user_ref_to_sites.rb
+++ db/migrate/20180611212110_add_user_ref_to_sites.rb
@@ -1,5 +1,5 @@
 class AddUserRefToSites < ActiveRecord::Migration[5.2]
   def change
-    add_reference :site, :user, foreign_key: true
+    add_reference :sites, :user, foreign_key: true
   end
 end
